3af2d93d97e0b292c41d2055e03f5e2aaaa33593
Alexandr Topilski <atopilski@gmail.com>
2020-05-13 21:53:08 +0300

    Update nginx_hls_debian.sh

--- build/nginx_hls_debian.sh
+++ build/nginx_hls_debian.sh
@@ -4,5 +4,5 @@ set -ex
 # update system
 apt-get update
 apt-get install -y nginx
-cp nginx/default /etc/nginx/sites-available/default
+cp env/nginx/fastocloud /etc/nginx/sites-available/fastocloud
 systemctl restart nginx
